2bf58edc5b555bef03fac681224acfd351892814
juancorreaxsmarts <juan.correa@xmarts.com>
2022-07-01 15:10:05 -0400

    cambios en product

--- iberica_mod_transfer/models/models.py
+++ iberica_mod_transfer/models/models.py
@@ -18,7 +18,7 @@ class Picking(models.Model):
         for line in self:
             if self.product_id:
                 self.tara = line.picking_id.tara
-                self.peso_bruto_x = line.product_id.weight
+                self.peso_bruto_x = line.picking_id.product_id.weight
                 if self.product_id.categ_id != 2288 :
                     self.peso_neto_x = self.peso_bruto_x * self.qty_done
                     self.peso_neto = self.peso_bruto - self.tara - self.peso_neto_x
@@ -29,5 +29,5 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     tara = fields.Float(default=1.0, store=True)
-    producto_terminado = fields.Many2one('product.product')
+    #producto_terminado = fields.Many2one('product.product')
     #product_id = fields.Many2one('product.product', 'Product', related='move_lines.product_id', readonly=True)

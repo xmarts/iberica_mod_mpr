acf7ea5870b3be082dd36f680834fa2b257c9231
juancorreaxsmarts <juan.correa@xmarts.com>
2022-07-07 21:27:30 -0400

    cambios decimales

--- iberica_mod_transfer/models/models.py
+++ iberica_mod_transfer/models/models.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from decimal import Decimal
 
 class Picking(models.Model):
     _inherit = 'stock.move.line'
     _description = 'test_module.test_module'
-    tara = fields.Float(default=1.0, store=True)
+    tara = fields.Float(default=1.0, store=True, digits=(12,3))
 
-    peso_bruto = fields.Float(default=1.0, store=True)
-    peso_neto = fields.Float(default=1.0, store=True)
+    peso_bruto = fields.Float(default=1.0, store=True, digits=(12,3))
+    peso_neto = fields.Float(default=1.0, store=True, digits=(12,3))
     semielaborado = fields.Boolean()
 
-    peso_neto_x = fields.Float(default=1.0, store=True)
-    peso_bruto_x = fields.Float(default=1.0, store=True)
+    peso_neto_x = fields.Float(default=1.0, store=True, digits=(12,3))
+    peso_bruto_x = fields.Float(default=1.0, store=True, digits=(12,3))
 
     @api.onchange('tara', 'peso_bruto', 'qty_done')
     def _traer_datos(self):
@@ -23,8 +22,8 @@ class Picking(models.Model):
                 self.peso_bruto_x = line.picking_id.product_id.peso_caja_presentacion
                 if self.product_id.semielaborado != True :
                     self.semielaborado = False
-                    self.peso_neto_x = Decimal(self.peso_bruto_x) * self.qty_done
-                    self.peso_neto = Decimal(self.peso_bruto - self.tara - self.peso_neto_x)
+                    self.peso_neto_x = self.peso_bruto_x * self.qty_done
+                    self.peso_neto = self.peso_bruto - self.tara - self.peso_neto_x
                 else:
                     self.semielaborado = True
                     self.qty_done = self.peso_bruto - self.tara
@@ -32,6 +31,6 @@ class Picking(models.Model):
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    tara = fields.Float(default=1.0, store=True)
+    tara = fields.Float(default=1.0, store=True, digits=(12,3))
     #producto_terminado = fields.Many2one('product.product')
     #product_id = fields.Many2one('product.product', 'Product', related='move_lines.product_id', readonly=True)
--- iberica_mod_transfer/models/transfer.py
+++ iberica_mod_transfer/models/transfer.py
@@ -8,7 +8,7 @@ class Picking(models.Model):
     _description = 'test_module.test_module'
 
 
-    peso_bruto = fields.Float(store=True)
+    peso_bruto = fields.Float(store=True, digits=(12,3))
 
     @api.onchange('shipping_weight')
     def _traer_datos(self):
@@ -20,5 +20,5 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
     _description = 'test_module.test_module'
 
-    peso_caja_presentacion = fields.Float(store=True)
+    peso_caja_presentacion = fields.Float(store=True, digits=(12,3))
     semielaborado = fields.Boolean()

8896d392c9ec496ce251d8cd85d04ad5e7a48512
juancorreaxsmarts <juan.correa@xmarts.com>
2022-07-13 03:14:04 -0400

    solved sqp

--- iberica_mod_transfer/models/models.py
+++ iberica_mod_transfer/models/models.py
@@ -32,3 +32,18 @@ class StockPicking(models.Model):
     tara = fields.Float(default=1.0, store=True, digits=(12,3))
     #producto_terminado = fields.Many2one('product.product')
     #product_id = fields.Many2one('product.product', 'Product', related='move_lines.product_id', readonly=True)
+
+    def action_put_in_pack(self):
+        res = super(Picking, self).action_put_in_pack()
+        peso_bruto = fields.Float(store=True, digits=(12,3))
+        peso_neto = fields.Float(store=True, digits=(12,3))
+        for line in self:
+            self.peso_bruto = line.move_line_ids.peso_bruto
+            self.peso_neto = line.move_line_ids.peso_neto
+        res.update({
+            #'quantity': self.product_uom_qty,
+            'shipping_weight': self.peso_neto,
+            'peso_bruto': self.peso_bruto,
+
+        })
+        return res
--- iberica_mod_transfer/models/transfer.py
+++ iberica_mod_transfer/models/transfer.py
@@ -3,18 +3,12 @@
 from odoo import models, fields, api
 
 
-class Picking(models.Model):
+class PickingQuant(models.Model):
     _inherit = 'stock.quant.package'
     _description = 'test_module.test_module'
 
 
     peso_bruto = fields.Float(store=True, digits=(12,3))
-
-    @api.onchange('shipping_weight')
-    def _traer_datos(self):
-        if self.shipping_weight:
-            self.shipping_weight = self.env['stock.move.line'].browse(vals.get('peso_neto'))
-            self.peso_bruto = self.env['stock.move.line'].browse(vals.get('peso_bruto'))
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -22,3 +16,4 @@ class ProductTemplate(models.Model):
 
     peso_caja_presentacion = fields.Float(store=True, digits=(12,3))
     semielaborado = fields.Boolean()
+

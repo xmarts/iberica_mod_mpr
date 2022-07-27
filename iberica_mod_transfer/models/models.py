dd1977b3813547565195b5b9b2468b289df0c0d6
danielarochexmarts <daniela.roche.xmarts@gmail.com>
2022-07-27 11:34:06 -0400

    [FIX] peso_tara_name

--- iberica_mod_transfer/models/models.py
+++ iberica_mod_transfer/models/models.py
@@ -6,8 +6,8 @@ from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 class Picking(models.Model):
     _inherit = 'stock.move.line'
     _description = 'test_module.test_module'
-    tara = fields.Float(default=1.0, store=True, digits=(12,3))
-
+    
+    peso_tara = fields.Float(default=1.0, store=True, digits=(12,3))
     peso_bruto = fields.Float(default=1.0, store=True, digits=(12,3))
     peso_neto = fields.Float(default=1.0, store=True, digits=(12,3))
     semielaborado = fields.Boolean()
@@ -16,25 +16,25 @@ class Picking(models.Model):
     peso_neto_x = fields.Float(default=1.0, store=True, digits=(12,3))
     peso_bruto_x = fields.Float(default=1.0, store=True, digits=(12,3))
 
-    @api.onchange('tara', 'peso_bruto', 'qty_done', 'product_id')
+    @api.onchange('peso_tara', 'peso_bruto', 'qty_done', 'product_id')
     def _traer_datos(self):
         for line in self:
             if self.product_id:
                 self.picking_relacion = line.picking_id.relacion
-                self.tara = line.picking_id.tara
+                self.peso_tara = line.picking_id.peso_tara
                 self.peso_bruto_x = line.picking_id.product_id.peso_caja_presentacion
                 self.semielaborado_x = line.product_id.semielaborado
                 self.semielaborado = self.semielaborado_x
                 if self.semielaborado != True :
                     self.peso_neto_x = self.peso_bruto_x * self.qty_done
-                    self.peso_neto = self.peso_bruto - self.tara - self.peso_neto_x
+                    self.peso_neto = self.peso_bruto - self.peso_tara - self.peso_neto_x
                 else:
-                    self.qty_done = self.peso_bruto - self.tara
+                    self.qty_done = self.peso_bruto - self.peso_tara
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    tara = fields.Float(default=1.0, store=True, digits=(12,3))
+    peso_tara = fields.Float(default=1.0, store=True, digits=(12,3))
     relacion = fields.Char(related="picking_type_id.barcode")
     #default = fields.Char(default="WIP-INTERNAL")
     peso_bruto = fields.Float(store=True, digits=(12,3))

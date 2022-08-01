18c08ccf9116e19297933335223e0caa699b5d81
root <juancorreaxsmarts>
2022-08-01 13:40:25 -0400

    ultimo commit 1eroago

--- iberica_mod_transfer/models/models.py
+++ iberica_mod_transfer/models/models.py
@@ -1,24 +1,54 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 class Picking(models.Model):
-    _inherit = 'stock.move'
+    _inherit = 'stock.move.line'
     _description = 'test_module.test_module'
-    tara = fields.Float(default=1.0, store=True)
-    peso_bruto = fields.Float(default=1.0, store=True)
-    peso_neto = fields.Float(default=1.0, store=True)
+    
+    peso_tara = fields.Float(string="Tara", default=0.0, store=True, digits=(12,3))
+    peso_bruto = fields.Float(default=0.0, store=True, digits=(12,3))
+    peso_neto = fields.Float(default=0.0, store=True, digits=(12,3))
+    semielaborado = fields.Boolean()
+    semielaborado_x = fields.Boolean()
+    picking_relacion = fields.Integer(related="picking_id.relacion")
+    peso_neto_x = fields.Float(default=1.0, store=True, digits=(12,3))
+    peso_bruto_x = fields.Float(default=1.0, store=True, digits=(12,3))
+    tara = fields.Float(default=1.0, store=True, digits=(12,3))
 
-    peso_neto_x = fields.Float(default=1.0, store=True)
-    peso_bruto_x = fields.Float(default=1.0, store=True)
-
-    @api.onchange('tara', 'peso_bruto', 'quantity_done')
+    @api.onchange('peso_tara', 'peso_bruto', 'qty_done', 'product_id')
     def _traer_datos(self):
         for line in self:
-            if self.product_id:
-                self.peso_bruto_x = line.product_id.weight
-                if self.product_id.categ_id != 2 :
-                    self.peso_neto_x = self.peso_bruto_x * self.quantity_done
-                    self.peso_neto = self.tara - self.peso_bruto - self.peso_neto_x
+            if line.product_id:
+                line.picking_relacion = line.picking_id.relacion
+                line.peso_tara = line.picking_id.peso_tara
+                line.peso_bruto_x = line.picking_id.product_id.peso_caja_presentacion
+                line.semielaborado_x = line.product_id.semielaborado
+                line.semielaborado = line.semielaborado_x
+                if line.semielaborado != True :
+                    line.peso_neto_x = line.peso_bruto_x * line.qty_done
+                    line.peso_neto = line.peso_bruto - line.peso_tara - line.peso_neto_x
                 else:
-                    self.quantity_done = felf.peso_bruto - self.tara
+                    if line.peso_bruto != 0:
+                        line.qty_done = line.peso_bruto - line.peso_tara
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    peso_tara = fields.Float(default=1.0, store=True, digits=(12,3))
+    relacion = fields.Integer(related="picking_type_id.id")
+    peso_bruto = fields.Float(store=True, digits=(12,3))
+    peso_neto = fields.Float(store=True, digits=(12,3))
+    tara = fields.Float(default=1.0, store=True, digits=(12,3))
+
+    @api.depends('picking_type_id')
+    def _traer_rel(self):
+        for r in self:
+            if r.picking_type_id:
+                r.relacion = r.picking_type_id.barcode
+    
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    barcode = fields.Char("Código de barra")
--- iberica_mod_transfer/models/transfer.py
+++ iberica_mod_transfer/models/transfer.py
@@ -1,16 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
-class Picking(models.Model):
+class StockQuantPackage(models.Model):
     _inherit = 'stock.quant.package'
+    #_description = 'test_module.test_module'
+
+    @api.depends('quant_ids')
+    def _compute_weight_bruto(self):
+        for package in self:
+            peso_bruto = 0.0
+            if self.env.context.get('picking_id'):
+                # TODO: potential bottleneck: N packages = N queries, use groupby ?
+                current_picking_move_line_ids = self.env['stock.move.line'].search([
+                    ('result_package_id', '=', package.id),
+                    ('picking_id', '=', self.env.context['picking_id'])
+                ])
+                for ml in current_picking_move_line_ids:
+                    peso_bruto += ml.peso_bruto
+            package.peso_bruto = peso_bruto
+
+    peso_bruto = fields.Float(compute='_compute_weight_bruto', store=True, digits=(12,3))
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
     _description = 'test_module.test_module'
 
+    peso_caja_presentacion = fields.Float(store=True, digits=(12,3))
+    semielaborado = fields.Boolean()
 
-    peso_bruto = fields.Float(store=True)
-
-    @api.onchange('shipping_weight')
-    def _traer_datos(self):
-        if self.shipping_weight:
-            self.peso_bruto = self.shipping_weight

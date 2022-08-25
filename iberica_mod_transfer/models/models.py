2f7b04eb8a08a827290c1915d1d7581f6874d1d2
danielarochexmarts <daniela.roche.xmarts@gmail.com>
2022-08-25 17:49:13 -0400

    [FIX] modificacion de domain

--- iberica_mod_transfer/models/models.py
+++ iberica_mod_transfer/models/models.py
@@ -21,7 +21,7 @@ class Picking(models.Model):
         string="Paquete origen", 
         ondelete='restrict',
         check_company=True,
-        domain="[('product_id', 'in', parent.move_line_ids.package_id.producto_id)]"
+        domain="[('product_id', 'in', parent.product_id.quant_ids)]"
     )
 
     @api.onchange('peso_tara', 'peso_bruto', 'qty_done', 'product_id')
